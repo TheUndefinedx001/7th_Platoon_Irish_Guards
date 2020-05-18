01097280b14bebd64df61ecb728445492063632b
TheUndefinedx001 <64269680+TheUndefinedx001@users.noreply.github.com>
2020-05-18 11:47:03 +0200

    Woops

--- synclap.bat
+++ synclap.bat
@@ -1,7 +1,7 @@
 @echo off
 echo This will sync all settings on Leechers PC. If you do not have the same configuration, you should not be running this program!.
 pause
-Xcopy /Y /E /I . "C:\Users\%username%\Desktop\SteamCMD\arma-ds\"
+Xcopy /Y /E /I ".\Server files\" "C:\Users\%username%\Desktop\SteamCMD\arma-ds\"
 pause
 echo Syncing complete.
 pause
