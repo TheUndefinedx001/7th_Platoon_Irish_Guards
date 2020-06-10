1873082f87c847bf36620b084b1944d148eb6b0e
TheUndefinedx001 <64269680+TheUndefinedx001@users.noreply.github.com>
2020-06-10 10:02:54 +0200

    CBA settings applied to all files + modpack hotfix

--- sync_leecher_pc.bat
+++ sync_leecher_pc.bat
@@ -2,8 +2,8 @@
 echo This will sync all settings on Leechers PC. If you do not have the same configuration, you should not be running this program!.
 pause
 Xcopy /Y /E /I "C:\Users\%username%\Documents\Arma 3 - Other Profiles\[Sgt%%2e]%%20Leecher\mpmissions" "C:\Users\%username%\Documents\GitHub\7th_Platoon_Irish_Guards\Editor files\"
-xcopy /Y /E /I "D:\SteamLibrary\steamapps\common\Arma 3\MPMissions" "C:\Users\%username%\Documents\GitHub\7th_Platoon_Irish_Guards\Server files\mpmissions\"
-Xcopy /Y /E /I "C:\Users\%username%\Documents\GitHub\7th_Platoon_Irish_Guards\Server files" "D:\SteamLibrary\steamapps\common\Arma 3\"
+xcopy /Y /E /I "E:\SteamLibrary\steamapps\common\Arma 3\MPMissions" "C:\Users\%username%\Documents\GitHub\7th_Platoon_Irish_Guards\Server files\mpmissions\"
+Xcopy /Y /E /I "C:\Users\%username%\Documents\GitHub\7th_Platoon_Irish_Guards\Server files" "E:\SteamLibrary\steamapps\common\Arma 3\"
 pause
 cls
 echo Syncing complete.
